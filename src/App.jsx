3abd923fdce9a50c9b4ba76e4f4c05c78ee5094e
Prakhar <mishraprakhar150@gmail.com>
2024-07-16 03:08:13 +0530

    Added extra features

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,6 @@
 import { useState,useEffect } from 'react'
 import Searchbar from './Searchbar';
 function App() {
-  // const [data,setData] = useState([]);
-  // useEffect(()=>{
-  //   fetch('https://codeforces.com/api/problemset.problems')
-  // .then((result)=>{result.json()
-  //   .then((res) => {
-  //     setData(res);
-  //   })
-  //   .catch((err)=>{
-  //     console.log(`An error has occured: ${err}`);
-  //   })
-  // })
-  // },[])
-  
-
   return (
     <>
       <Searchbar></Searchbar>
--- src/Searchbar.jsx
+++ src/Searchbar.jsx
@@ -1,54 +1,62 @@
-import React, { useState, useEffect } from 'react';
-
+import React from 'react'
+import {useState, useEffect} from 'react'
 function Searchbar() {
-    const [query, setQuery] = useState('');
-    const [problems, setProblems] = useState([]);
-
-    const fetchData = async (value) => {
-        try {
+    const [problems,setProblems] = useState([]);
+    const [query,setQuery] = useState('');
+    const [rating,setRating] = useState(800);
+    const fetchData = async (val, rate)=>{
+        try{
             const response = await fetch('https://codeforces.com/api/problemset.problems');
             const data = await response.json();
-            const prob = data.result.problems.filter((problem) => {
-                return (
-                    value &&
+            const prob = data.result.problems.filter((problem)=>{
+                return(
+                    val &&
+                    rate &&
                     problem &&
-                    problem.tags &&
-                    problem.tags.includes(value)
-                );
+                    problem.tags.includes(val) &&
+                    problem.rating == rate
+                )
             });
+            console.log(prob);
             setProblems(prob);
-        } catch (error) {
-            console.error('Error fetching data:', error);
         }
-    };
-
-    const handleChange = (value) => {
-        setQuery(value);
-    };
-
-    useEffect(() => {
-        if (query) {
-            fetchData(query);
-        } else {
+        catch(error){
+            console.error("Error in fetching data: " + error);
+        }
+    }
+    useEffect(()=>{
+        if(query,rating){
+            fetchData(query,rating);
+        }
+        else{
             setProblems([]);
         }
-    }, [query]);
-
-    return (
-        <div>
-            <input 
-                placeholder="Type to search tags..."
-                value={query}
-                onChange={(e) => handleChange(e.target.value)}
-            />
-            {problems.length > 0 && (
+    },[query,rating])
+    const handleChange = (value)=>{
+        setQuery(value);
+    }
+    const handleRating = (val)=>{
+        console.log(val);
+        setRating(val);
+    }
+  return (   
+    <>
+    <div>
+    <input 
+    name = "Tags"
+    value = {query}
+    placeholder='Tags'
+    onChange = {(e) => handleChange(e.target.value)}/>
+    </div>
+    <div>
+    <input type="range" min = "800" max = "3000" value={rating} step = "200" onChange = {(e)=>handleRating(e.target.value)}/>
+    </div>
+    {problems.length > 0 && (
                 <ul>
                     {problems.map((problem, index) => (
                         <li key={index}>
                             <a 
                                 href={`https://codeforces.com/contest/${problem.contestId}/problem/${problem.index}`}
-                                target="_blank"
-                                rel="noopener noreferrer"
                             >
                                 {problem.name}
                             </a>
@@ -56,8 +64,8 @@ function Searchbar() {
                     ))}
                 </ul>
             )}
-        </div>
-    );
+    </>
+  )
 }
 
-export default Searchbar;
+export default Searchbar
